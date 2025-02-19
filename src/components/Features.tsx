1d80aa7674cc188f93b95abb85f8428281eaaa18
Nesunder <nicobastien927@gmail.com>
2025-02-19 16:08:14 -0300

    Restrict Features's  paragraph width, center it, add padding and increse h2 font size when the screen get larger.

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -23,12 +23,12 @@ const features = [
 
 export const Features = () => {
   return (
-    <div className="bg-black text-white py-[72px]">
+    <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
-        <h2 className="text-center font-bold text-5xl tracking-tighter">
+        <h2 className="text-center font-bold text-5xl sm:tex-6xl tracking-tighter">
           Everything you need
         </h2>
-        <div>
+        <div className="max-w-xl mx-auto">
           <p className="text-center mt-5 text-xl text-white/70">
             Enjoy customizable lists, team work tools, and smart tracking all in
             one place. Set tasks, get reminders, and see your progress simply
