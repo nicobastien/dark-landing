1731fb797a0a3f41baa1882ac281b84c3c03a5fa
Nesunder <nicobastien927@gmail.com>
2025-02-20 14:29:21 -0300

    Call to action section

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,3 +1,35 @@
+import helixImage from "../assets/images/helix2.png";
+import emojiStarImage from "../assets/images/emojistar.png";
+import Image from "next/image";
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24  text-center">
+      <div className="container max-w-xl relative">
+        <Image src={helixImage} className="absolute top-6 left-[calc(100%+36px)]" alt="" />
+        <Image src={emojiStarImage} className="absolute -top-[128px] right-[calc(100%+24px)]" alt="" />
+
+        <h2 className="font-bold text-5xl sm:text-6xl tracking-tighter">
+          Get instant access
+        </h2>
+        <p className="text-xl text-white/70 mt-5">
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts.
+        </p>
+        <form
+          className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row"
+          action=""
+        >
+          <input
+            className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9CA3AF] sm:flex-1"
+            type="email"
+            placeholder="your@email.com"
+          />
+          <button className="bg-white text-black h-12 rounded-lg px-5">
+            Get access
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
