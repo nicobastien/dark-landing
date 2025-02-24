919c13483678945c310a46d139e6e3febd7a44e1
Nesunder <nicobastien927@gmail.com>
2025-02-24 14:17:11 -0300

    Features animation

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,4 +1,4 @@
-import EcosystemIcon from "../assets/icons/ecosystem.svg";
+import { Feature } from "./Feature";
 
 const features = [
   {
@@ -38,16 +38,7 @@ export const Features = () => {
 
         <div className="flex flex-col sm:flex-row gap-4 mt-16">
           {features.map(({ id, title, description }) => (
-            <div
-              className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1"
-              key={id}
-            >
-              <div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
-                <EcosystemIcon />
-              </div>
-              <h3 className="mt-6 font-bold">{title}</h3>
-              <p className="mt-2 text-white/70">{description}</p>
-            </div>
+            <Feature title={title} description={description} key={id} />
           ))}
         </div>
       </div>
