8b0bd7037cde436f84d7949ca696ec711cc3dc6b
Nesunder <nicobastien927@gmail.com>
2025-02-24 14:18:12 -0300

    Product showcase animation

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,7 +1,20 @@
+"use client";
 import appScreen from "../assets/images/app-screen.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const appImage = useRef<HTMLImageElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: appImage,
+    offset: ["start end", "end end"],
+  });
+
+  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+
+
   return (
     <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] sm:py-24">
       <div className="container">
@@ -15,7 +28,20 @@ export const ProductShowcase = () => {
             one task at a time.
           </p>
         </div>
-        <Image className="mt-14 mx-auto" src={appScreen} alt="The product screenshot" />
+        <motion.div
+          style={{
+            opacity: opacity,
+            rotateX: rotateX,
+            transformPerspective: "800px",
+          }}
+        >
+          <Image
+            className="mt-14 mx-auto"
+            src={appScreen}
+            alt="The product screenshot"
+            ref={appImage}
+          />
+        </motion.div>
       </div>
     </div>
   );
